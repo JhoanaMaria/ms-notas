12ae8d0e2d76c6b4c961e0ae875839f177d9e412
Maria <jhoanamariarp@ufps.edu.co>
2023-06-23 08:02:57 -0500

    cambios

--- app/note/update/service.js
+++ app/note/update/service.js
@@ -1,6 +1,12 @@
 const { db } = require("../../repositories/mysql/getConnection")
 
 async function updateNoteById(id, note) {
+  let savedNote = db.note.findByPk(id, { raw: true })
+
+  if (savedNote.status !== note.status) {
+    note.statusUpdatedAt = Date.now()
+  }
+
   let noteUpdated = await db.note.update(note, {
     where: { id },
   })
